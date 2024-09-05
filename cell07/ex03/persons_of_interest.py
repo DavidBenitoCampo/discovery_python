bc9754d8102d393797e39a2de2006d98dd478626
bocal <bocal@c2r10s5.42prague.com>
2024-09-05 19:11:59 +0200

    Last function

---
+++ cell07/ex03/persons_of_interest.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+# your method definition here
+women_scientists = {
+"ada": { "name": "Ada Lovelace", "date_of_birth": "1815" },
+"cecilia": { "name": "Cecila Payne", "date_of_birth": "1900" },
+"lise": { "name": "Lise Meitner", "date_of_birth": "1878" },
+"grace": { "name": "Grace Hopper", "date_of_birth": "1906" }
+}
+
+
+
+def famous_births():
+    famous = []
+    for name, date_of_birth in a.items():
+        famous.append(name, date_of_birth)
+    return famous
+
+
+
+
+
+
+
+print(famous_births)
