3124497ecc73389c36552d3e591255a52333c5ae
bocal <bocal@c2r10s5.42prague.com>
2024-09-06 14:03:20 +0200

    Last function

--- cell07/ex03/persons_of_interest.py
+++ cell07/ex03/persons_of_interest.py
@@ -2,24 +2,26 @@
 
 # your method definition here
 women_scientists = {
-"ada": { "name": "Ada Lovelace", "date_of_birth": "1815" },
-"cecilia": { "name": "Cecila Payne", "date_of_birth": "1900" },
-"lise": { "name": "Lise Meitner", "date_of_birth": "1878" },
-"grace": { "name": "Grace Hopper", "date_of_birth": "1906" }
+    "ada": {"name": "Ada Lovelace", "date_of_birth": "1815" },
+    "cecilia": {"name": "Cecila Payne", "date_of_birth": "1900" },
+    "lise": {"name": "Lise Meitner", "date_of_birth": "1878" },
+    "grace": {"name": "Grace Hopper", "date_of_birth": "1906" }
 }
 
 
 
-def famous_births():
-    famous = []
-    for name, date_of_birth in a.items():
-        famous.append(name, date_of_birth)
-    return famous
+# Sort by date_of_birth and create a dictionary
+famous = [(name, date_of_birthday) for name, date_of_birthday in women_scientists.items()]
+scientists = sorted(famous, key=lambda x: x[1]["date_of_birth"])
+dictionary = dict(scientists)
+
+# Function to print out famous scientists
+def famous_births(dictionary):
+    for i, j in dictionary.items():
+        print(f"{j['name']} is a great scientist born in {j['date_of_birth']}")
 
 
 
 
 
-
-
-print(famous_births)
+famous_births(dictionary)
